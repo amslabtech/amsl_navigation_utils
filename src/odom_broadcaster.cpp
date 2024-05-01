0823028b88567a6b51c3c3f7c182bb39fdadd850
Toshiki Nakamura <82020865+ToshikiNakamura0412@users.noreply.github.com>
2024-05-01 11:23:49 +0900

    Add roslint (#18)

--- src/odom_broadcaster.cpp
+++ src/odom_broadcaster.cpp
@@ -1,61 +1,55 @@
-#include <ros/ros.h>
-#include <nav_msgs/Odometry.h>
-#include <tf/transform_broadcaster.h>
+// Copyright 2023 amsl
 
+#include <nav_msgs/Odometry.h>
+#include <ros/ros.h>
+#include <tf/transform_broadcaster.h>
 
 class OdomBroadcaster
 {
 public:
-    OdomBroadcaster(void);
-    void process();
+  OdomBroadcaster(void);
+  void process();
 
 protected:
-    void odom_callback(const nav_msgs::Odometry::ConstPtr& msg);
+  void odom_callback(const nav_msgs::Odometry::ConstPtr &msg);
 
-    ros::NodeHandle nh_;
-    ros::Subscriber odom_sub_;
-    tf::TransformBroadcaster *odom_broadcaster = nullptr;
+  ros::NodeHandle nh_;
+  ros::Subscriber odom_sub_;
+  tf::TransformBroadcaster *odom_broadcaster = nullptr;
 };
-
 
 OdomBroadcaster::OdomBroadcaster()
 {
-    odom_sub_ = nh_.subscribe("odom", 1, &OdomBroadcaster::odom_callback, this);
-    odom_broadcaster = new tf::TransformBroadcaster;
-    ROS_INFO_STREAM("=== Odom Broadcaster ===");
+  odom_sub_ = nh_.subscribe("odom", 1, &OdomBroadcaster::odom_callback, this);
+  odom_broadcaster = new tf::TransformBroadcaster;
+  ROS_INFO_STREAM("=== Odom Broadcaster ===");
 }
 
-
-void OdomBroadcaster::odom_callback(const nav_msgs::Odometry::ConstPtr& msg)
+void OdomBroadcaster::odom_callback(const nav_msgs::Odometry::ConstPtr &msg)
 {
-    geometry_msgs::TransformStamped odom_trans;
+  geometry_msgs::TransformStamped odom_trans;
 
-    odom_trans.header.stamp = msg->header.stamp;
-    odom_trans.header.frame_id = msg->header.frame_id;
-    odom_trans.child_frame_id = msg->child_frame_id;
-    odom_trans.transform.translation.x = msg->pose.pose.position.x;
-    odom_trans.transform.translation.y = msg->pose.pose.position.y;
-    odom_trans.transform.translation.y = msg->pose.pose.position.z;
-    odom_trans.transform.rotation.x    = msg->pose.pose.orientation.x;
-    odom_trans.transform.rotation.y    = msg->pose.pose.orientation.y;
-    odom_trans.transform.rotation.z    = msg->pose.pose.orientation.z;
-    odom_trans.transform.rotation.w    = msg->pose.pose.orientation.w;
+  odom_trans.header.stamp = msg->header.stamp;
+  odom_trans.header.frame_id = msg->header.frame_id;
+  odom_trans.child_frame_id = msg->child_frame_id;
+  odom_trans.transform.translation.x = msg->pose.pose.position.x;
+  odom_trans.transform.translation.y = msg->pose.pose.position.y;
+  odom_trans.transform.translation.y = msg->pose.pose.position.z;
+  odom_trans.transform.rotation.x = msg->pose.pose.orientation.x;
+  odom_trans.transform.rotation.y = msg->pose.pose.orientation.y;
+  odom_trans.transform.rotation.z = msg->pose.pose.orientation.z;
+  odom_trans.transform.rotation.w = msg->pose.pose.orientation.w;
 
-    odom_broadcaster->sendTransform(odom_trans);
+  odom_broadcaster->sendTransform(odom_trans);
 }
 
-
-void OdomBroadcaster::process()
-{
-    ros::spin();
-}
-
+void OdomBroadcaster::process() { ros::spin(); }
 
 int main(int argc, char **argv)
 {
-    ros::init(argc, argv, "odom_broadcaster");
-    OdomBroadcaster odom_broadcaster;
-    odom_broadcaster.process();
+  ros::init(argc, argv, "odom_broadcaster");
+  OdomBroadcaster odom_broadcaster;
+  odom_broadcaster.process();
 
-    return 0;
+  return 0;
 }
--- src/scan_invertor.cpp
+++ src/scan_invertor.cpp
@@ -1,42 +1,44 @@
+// Copyright 2023 amsl
+
 #include <ros/ros.h>
 #include <sensor_msgs/LaserScan.h>
 
 class ScanInvertor
 {
 public:
-    ScanInvertor(void);
-    void process();
+  ScanInvertor(void);
+  void process();
 
 private:
-    void scan_callback(const sensor_msgs::LaserScan::ConstPtr &scan);
+  void scan_callback(const sensor_msgs::LaserScan::ConstPtr &scan);
 
-    ros::NodeHandle nh_;
-    ros::Publisher inverted_scan_pub_;
-    ros::Subscriber scan_sub_;
+  ros::NodeHandle nh_;
+  ros::Publisher inverted_scan_pub_;
+  ros::Subscriber scan_sub_;
 };
 
 ScanInvertor::ScanInvertor()
 {
-    inverted_scan_pub_ = nh_.advertise<sensor_msgs::LaserScan>("/scan/inverse", 1);
-    scan_sub_ = nh_.subscribe("/scan", 1, &ScanInvertor::scan_callback, this);
-    ROS_INFO_STREAM("=== Scan Invertor ===");
+  inverted_scan_pub_ = nh_.advertise<sensor_msgs::LaserScan>("/scan/inverse", 1);
+  scan_sub_ = nh_.subscribe("/scan", 1, &ScanInvertor::scan_callback, this);
+  ROS_INFO_STREAM("=== Scan Invertor ===");
 }
 
 void ScanInvertor::scan_callback(const sensor_msgs::LaserScan::ConstPtr &msg)
 {
-    sensor_msgs::LaserScan inverted_scan = *msg;
-    for (int i = 0; i < msg->ranges.size(); i++)
-        inverted_scan.ranges[i] = msg->ranges[msg->ranges.size() - 1 - i];
-    inverted_scan_pub_.publish(inverted_scan);
+  sensor_msgs::LaserScan inverted_scan = *msg;
+  for (int i = 0; i < msg->ranges.size(); i++)
+    inverted_scan.ranges[i] = msg->ranges[msg->ranges.size() - 1 - i];
+  inverted_scan_pub_.publish(inverted_scan);
 }
 
 void ScanInvertor::process() { ros::spin(); }
 
 int main(int argc, char **argv)
 {
-    ros::init(argc, argv, "scan_invertor");
-    ScanInvertor scan_invertor;
-    scan_invertor.process();
+  ros::init(argc, argv, "scan_invertor");
+  ScanInvertor scan_invertor;
+  scan_invertor.process();
 
-    return 0;
+  return 0;
 }
